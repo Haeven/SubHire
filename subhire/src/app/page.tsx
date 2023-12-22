a881bcbee87aeb72cc6f11cfd7f2fc01e0a3d325
Haeven Kelley <haevenshane@gmail.com>
2023-12-22 16:19:34 -0600

    Adds homepage banner

--- subhire/src/app/page.tsx
+++ subhire/src/app/page.tsx
@@ -1,10 +1,51 @@
 // import Image from 'next/image'
 import styles from './page.module.css'
+import Link from "next/link"
+/**
+ * v0 by Vercel.
+ * @see https://v0.dev/t/uneycOKChPd
+ */
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      We are SubHire.
-    </main>
-  )
-}
+ export default function Home() {
+   return (
+     <>
+       <section className="w-full py-12">
+         <div className="container px-4 md:px-6">
+           <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
+             <img
+               alt="SubHire"
+               className="mx-auto aspect-video overflow-hidden rounded-xl object-bottom sm:w-full lg:order-last lg:aspect-square"
+               height="550"
+               src="/placeholder.svg"
+               width="550"
+             />
+             <div className="flex flex-col justify-center space-y-4">
+               <div className="space-y-2">
+                 <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">Welcome to SubHire</h1>
+                 <p className="max-w-[600px] text-gray-500 md:text-xl dark:text-gray-400">
+                   The platform to meet all your subcontracting needs.
+                 </p>
+               </div>
+               <div className="flex flex-col gap-2 min-[400px]:flex-row">
+                 <Link
+                   className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+                   href="#"
+                 >
+                   Get Started
+                 </Link>
+                 <Link
+                   className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
+                   href="#"
+                 >
+                   Contact Us
+                 </Link>
+               </div>
+             </div>
+           </div>
+         </div>
+       </section>
+     </>
+   )
+ }
+ 
+ 
