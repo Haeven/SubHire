125635ee645add889d150d589e431047a6784aec
Haeven Kelley <haevenshane@gmail.com>
2023-12-26 13:17:50 -0600

    Adds layout page

--- subhire/src/app/layout.tsx
+++ subhire/src/app/layout.tsx
@@ -1,8 +1,22 @@
-import type { Metadata } from 'next'
+import { lazy, StrictMode, Suspense, useEffect } from "react";
+// import { AnimatePresence, motion } from "framer-motion";
+import { useAppDispatch, useAppSelector, useLocalStorage } from "@/lib/hooks";
+import dynamic from 'next/dynamic'
+// import { useAuthState } from "react-firebase-hooks/auth";
+// import { doc, onSnapshot, updateDoc } from "firebase/firestore";
+import { getPendingMsg, getToastMsg } from "@/toastSlice";
+import type { Metadata } from 'next'  
 import { Inter } from 'next/font/google'
 import './globals.css'
-
+import { LoadingSpinner, Toast } from "@/components";
+import { Providers } from '@/lib/redux/provider';
+import { getUserState } from "@/components/features/authentication/userSlice";
 const inter = Inter({ subsets: ['latin'] })
+import {
+  enableDarkmode,
+  getThemeState,
+  toggleDarkmode,
+} from "@/components/features/sidebar/themeSlice";
 
 export const metadata: Metadata = {
   title: 'SubHire',
@@ -14,9 +28,49 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  // const { user: currentUser } = useAppSelector(getUserState);
+
+  // const pendingMsg = useAppSelector(getPendingMsg);
+  // const toastMsg = useAppSelector(getToastMsg);
+  // const { darkmode } = useAppSelector(getThemeState);
+  const FallBack = <div className="h-screen w-screen flex justify-center items-center">
+    <LoadingSpinner msg="Loading..." />
+  </div>
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    // <Providers>
+      <html lang="en">
+        <body className={inter.className}>
+          <StrictMode>
+            {/* <AnimatePresence> */}
+              {/* Loading Toast */}
+              {/* {pendingMsg && <Toast type="loading" msg={pendingMsg} />} */}
+
+              {/* Notification Toast */}
+              {/* {toastMsg && <Toast durationMS={3000} msg={toastMsg} />} */}
+            {/* </AnimatePresence> */}
+
+            <Suspense fallback={FallBack}>
+              {/* {currentUser.uid ? ( */}
+                {/* <motion.div
+                  className="flex"
+                  animate={{ opacity: 1, x: 0, y: 0 }}
+                  initial={{ opacity: 0 }}
+                  exit={{ opacity: 0 }}
+                > */}
+                <Providers>
+                  {children}
+                </Providers>
+                {/* </motion.div> */}
+              {/* ) : (
+                <Authentication
+                  keepSignedIn={keepSignedIn}
+                  setKeepSignedIn={setKeepSignedIn}
+                />
+              )} */}
+            </Suspense>
+          </StrictMode>
+        </body>
+      </html>
+    // </Providers>
   )
 }
